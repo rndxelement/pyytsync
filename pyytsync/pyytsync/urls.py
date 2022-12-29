d362d344fb5b2997dd6b9499844a82555ebf81a5
Philipp Rösner <philipp.roesner@student.uni-halle.de>
2022-12-29 21:51:26 +0100

    move /sync url paths to /

--- pyytsync/pyytsync/urls.py
+++ pyytsync/pyytsync/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('sync/', include('sync.urls')),
+    path('', include('sync.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
